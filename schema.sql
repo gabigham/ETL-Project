73b6b91ebb60835cb50ac54a46e26573be621923
Dawn <brenner95670@gmail.com>
2019-09-16 20:56:58 -0700

    final

--- schema.sql
+++ schema.sql
@@ -1,19 +1,16 @@
-CREATE TABLE "country" (
+CREATE TABLE "countries" (
 	"country_id" serial NOT NULL,
-	"region" serial(255) NOT NULL,
-	CONSTRAINT "country_pk" PRIMARY KEY ("country_id")
-) WITH (
-  OIDS=FALSE
+	"region" TEXT,
+	"country" TEXT,
+	PRIMARY KEY ("country_id")
 );
 
 
 
 CREATE TABLE "population" (
 	"country_id" int NOT NULL,
-	"year" int NOT NULL,
-	"population" int NOT NULL
-) WITH (
-  OIDS=FALSE
+	"year" int,
+	"population" int
 );
 
 
@@ -21,47 +18,38 @@ CREATE TABLE "population" (
 CREATE TABLE "country_lookup" (
 	"name" TEXT NOT NULL,
 	"country_id" int NOT NULL
-) WITH (
-  OIDS=FALSE
 );
 
 
 
 CREATE TABLE "suicide" (
 	"country_id" int NOT NULL,
-	"year" int NOT NULL,
-	"suicide_no" int NOT NULL,
-	"hdi" int NOT NULL
-) WITH (
-  OIDS=FALSE
+	"year" int,
+	"suicide_no" int
 );
 
 
 
 CREATE TABLE "happiness" (
 	"country_id" int NOT NULL,
-	"year" int NOT NULL,
-	"happiness_rank" int NOT NULL,
-	"happiness_score" int NOT NULL,
-	"standard_error" int NOT NULL,
-	"economy" int NOT NULL,
-	"family" int NOT NULL,
-	"health" int NOT NULL,
-	"freedom" int NOT NULL,
-	"trust" int NOT NULL,
-	"generousity" int NOT NULL,
-	"dystopia_residual" int NOT NULL
-) WITH (
-  OIDS=FALSE
+	"year" int,
+	"happiness_rank" int,
+	"happiness" float4,
+	"economy" float4,
+	"family" float4,
+	"health" float4,
+	"freedom" float4,
+	"trust" float4,
+	"generosity" float4,
+	"dystopia" float4
 );
 
 
 
 
-ALTER TABLE "population" ADD CONSTRAINT "population_fk0" FOREIGN KEY ("country_id") REFERENCES "country"("country_id");
+ALTER TABLE "population" ADD CONSTRAINT "population_fk0" FOREIGN KEY ("country_id") REFERENCES "countries"("country_id");
 
 
-ALTER TABLE "suicide" ADD CONSTRAINT "suicide_fk0" FOREIGN KEY ("country_id") REFERENCES "country"("country_id");
+ALTER TABLE "suicide" ADD CONSTRAINT "suicide_fk0" FOREIGN KEY ("country_id") REFERENCES "countries"("country_id");
 
-ALTER TABLE "happiness" ADD CONSTRAINT "happiness_fk0" FOREIGN KEY ("country_id") REFERENCES "country"("country_id");
-
+ALTER TABLE "happiness" ADD CONSTRAINT "happiness_fk0" FOREIGN KEY ("country_id") REFERENCES "countries"("country_id");
